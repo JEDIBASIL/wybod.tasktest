fd0079307fb6b12250152b261407ea8607244365
JEDIBASIL <jedidiahbasil@gmail.com>
2025-10-19 22:49:57 +0100

    add pagination and filtering methods to ITaskRepository

--- Wybod.TaskTest/Interface/ITaskRepository.cs
+++ Wybod.TaskTest/Interface/ITaskRepository.cs
@@ -1,10 +1,20 @@
 using Wybod.TaskTest.Data.Models;
+using Wybod.TaskTest.DTOs;
 
 public interface ITaskRepository
 {
     IEnumerable<TaskItem> GetAll();
     TaskItem? GetById(Guid id);
+    PaginatedResult<TaskItem> GetAllPaginated(int pageNumber = 1, int pageSize = 10);
+    PaginatedResult<TaskItem> GetFiltered(
+        int pageNumber = 1, 
+        int pageSize = 10, 
+        bool? isCompleted = null, 
+        DateTime? startDate = null, 
+        DateTime? endDate = null);
+    IEnumerable<TaskItem> Search(string searchTerm);
     TaskItem Create(TaskItem task);
     bool Update(Guid id, TaskItem task);
     bool Delete(Guid id);
+    bool DeleteCompleted();
 }
