f66867a56ef464628035c4d948ebccb6e434a0a1
JEDIBASIL <jedidiahbasil@gmail.com>
2025-10-20 02:39:17 +0100

    added toast component to app

--- Wybod.TaskTest/ClientApp/src/App.vue
+++ Wybod.TaskTest/ClientApp/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <router-view />
+  <Toaster position="bottom-left" />
 </template>
 
 <script setup lang="ts">
+import { Toaster } from '@/components/ui/sonner'
+import 'vue-sonner/style.css'
 </script>
-
